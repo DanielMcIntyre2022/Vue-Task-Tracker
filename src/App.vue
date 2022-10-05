4cb385943af92e74ba9d24e5aad971070af2813a
Daniel <daniel-mcintyre@hotmail.com>
2022-10-05 15:51:42 -0400

    created and added header and button component within app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <h1>Hello world</h1>
+<div class="container">
+  <Header title="Task Tracker"/>
+</div>
 </template>
 
 <script>
 
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
